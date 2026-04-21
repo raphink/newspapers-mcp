bfab5ab3fa0edf6ef9f1a0242ad2a9916c31c7c4
Raphaël Pinson <raphael@isovalent.com>
2026-04-21 17:21:21 +0200

    Fix again
    Signed-off-by: Raphaël Pinson <raphael@isovalent.com>

--- src/providers/eluxemburgensia.ts
+++ src/providers/eluxemburgensia.ts
@@ -29,7 +29,7 @@ export async function searchEluxemburgensiaFull(query: string, date_from?: strin
     const pid = art.pid || "";
     const articleId = art.article || "";
     const pageId = art.begin || "";
-    const viewerUrl = pid ? `https://viewer.eluxemburgensia.lu/ark:${pid}/pages/1` : "";
+    const viewerUrl = pid ? `https://viewer.eluxemburgensia.lu/${pid}/pages/${pageId}` : "";
 
     // Build snippet coordinates from wordCoordinateSnippets
     const snippets = art.wordCoordinateSnippets || [];
